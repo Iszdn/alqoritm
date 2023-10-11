e5c4ebba5aa6890fce56dea1da2faa8286f05974
Orxan-Abdullazada <tu7pnc7lt@code.edu.az>
2023-10-11 13:49:42 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,107 @@
+// let a=10;
+// let b=20;
+// let c=8;
+// if(a>b && a>c){
+//     console.log("A boyuk");
+// }
+// else if(b>a && b>c){
+//     console.log("B boy");
+
+// }
+// else{
+//     console.log("c boy");
+// }
+
+// let i=18;
+// if(i%3===0 && i%7===0){
+//     console.log(`${i} ededi ikisinede bolunur`);
+// }
+// else if(i%7===0){
+//     console.log(`${i} ededi 7e bolunur`);
+// }
+// else if(i%3===0){
+//     console.log(`${i} ededi 3e bolunur`);
+// }
+// else{
+//     console.log(`${i} ededi hecbir halda bolunmur`);
+// }
+
+
+
+
+
+
+
+// for(let i=0; i<51; i++){
+//     console.log(i);
+// }
+
+
+// let i =0 ;
+
+// while(i<51){
+
+//     console.log(i);
+//      i++
+// }
+
+
+// --------------------------------------------
+// for(let i=50; i>0; i--){
+//     console.log(i);
+// }
+// -------------------------------------------
+// let sum=0;
+// for(let i=0 ; i<101; i++){
+//     sum += i;
+
+// } console.log(sum);
+// -----------------------------------------------
+// let hasil=1;
+// for(let i=1 ; i<11; i++){
+//     hasil *= i;
+
+// } console.log(hasil);
+// --------------------------------
+
+// const arr =["salam",1,4,"kenan"]
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+// const arr =["mehi","nura","mata","kenan"]
+// for (i=0; i<arr.length ; i++){
+// console.log(arr[i]);
+// }
+// ---------------------------------
+
+// const arr =["mehi","nura","mata","kenan"]
+// let NecefBurda=false;
+// for (i=0; i<arr.length ; i++){
+//      if (arr[i] === "Necef") {
+//           NecefBurda = true;
+//           console.log("necef burdadir");
+//           break;
+//      }
+//      }
+
+//      if(!NecefBurda){
+//           console.log("Necef burda deil");
+//      }
+     // ------------------------------
+
+// for(i=0; i<arr.length ; i++)
+
+// let a = 12345678901  + ""
+
+// for (i = 0; i < a.length; i++) {
+//     console.log(a[i]);
+// }
+
+// -----------------------------------------------------
+let arr=["a","b","b","a"]
+if (arr[0] === arr[3] && arr[1] === arr[2]) {
+     console.log("poilindrom");
+}
+else{
+     console.log("deil");
+}
